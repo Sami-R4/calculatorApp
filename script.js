104eaa130abdb659b1d45225daf452896dac5d60
sami-r4 <njuhsamuelrichmond@gmail.com>
2025-08-05 11:06:13 -0700

    UI editing

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Tab Logic
+const tabLinks = document.querySelectorAll('.tablinks');
+const tabContents = document.querySelectorAll('.tabcontents');
+
+// Toggling Active classes
+tabLinks.forEach(button => {
+    button.addEventListener('click', () => {
+        tabLinks.forEach(btn => 
+            btn.classList.remove('active'));
+        tabContents.forEach(content =>
+            content.classList.remove('active'));
+        button.classList.add('active');
+
+        const tabId = button.getAttribute('data-tab');
+        document.getElementById(tabId).classList.add('active');
+    });
+    
+   });
 
